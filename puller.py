73ed9160423eee778d2837db73fbeb483415d68b
Dmitriy Sinitskiy <ecosinica@gmail.com>
2020-01-13 17:55:58 +0300

    :rocket: add new frames for each biz

--- puller.py
+++ puller.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from time import strptime
+from datetime import datetime
 
 import settings
 
@@ -20,11 +22,11 @@ class BaseFinologBiz:
 
     def get_biz_accounts_API_url(self) -> str:
         """ Return link https://api.finolog.ru/v1/biz/{biz_id}/account """
-        return f'{self.FINOLOG_API_BIZ_URL}{self.biz_id}/account'
+        return '{}{}/account'.format(self.FINOLOG_API_BIZ_URL, self.biz_id)
 
-    def get_biz_transactions_API_url(self) ->:
+    def get_biz_transactions_API_url(self) -> str:
         """ Return link https://api.finolog.ru/v1/biz/{biz_id}/tramsaction """
-        return f'{self.FINOLOG_API_BIZ_URL}{self.biz_id}/transaction'
+        return '{}{}/transaction'.format(self.FINOLOG_API_BIZ_URL, self.biz_id)
 
     def get_accounts_response(self):
         return requests.get(self.get_biz_accounts_API_url(), headers={'Api-Token': settings.FINOLOG_API_KEY})
@@ -33,32 +35,68 @@ class BaseFinologBiz:
         return requests.get(self.get_biz_transactions_API_url(), headers={'Api-Token': settings.FINOLOG_API_KEY})
 
 
-class FinologBiz:
+class FinologBiz(BaseFinologBiz):
 
     """
     Class represents finolog.ru business instance.
-    Methods of class allow to build lametric frames form finilof API's requests.
+    Methods of class allow to build lametric frames form finolog API's requests.
 
     Frames for each business:
     1. Summary - sum from all accounts of biz;
     2. Income goal - goal frames with income's sum on current year;
     3. Income chart - chart frame with income by months.
     """
+    MONTHS_NUMBERS = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
 
-    def get_account_summary(self):
+    def get_summary_frame(self):
+        """ Example {"icon": "794", "text": "Hello!"} """
+        text = '{} K'.format(self._get_account_summary())
+        return dict(icon=self.icon, text=text, duration=self.duration)
+
+    def get_income_goal_frame(self):
+        """ Example  {"icon": "22835", "goalData": {"start": 0, "current": 6000, "end": 10000, "unit": "MI"}} """
+        current = self._get_income_transactions_sum_in_current_year()
+        result = {"icon": self.icon, "duration": self.duration, "goalData": {
+            "start": self.goal_start, "current": current, "end": self.goal_end, "unit": self.unit}}
+        return result
+
+    def get_income_chart_frame(self):
+        """ {"chartData": [1, 10, 15, 20, 6, 9, 11, 16, 22, 24]} """
+        chartData = []
+        values = self._get_income_transactions_sum_by_month_in_current_year()
+        for month in self.MONTHS_NUMBERS:
+            chartData.append(values[month])
+        return {'chartData': chartData}
+
+    def get_lametric_frames(self):
+        return self.get_summary_frame(), self.get_income_goal_frame(), self.get_income_chart_frame(),
+
+    def _get_account_summary(self) -> int:  # in thousands
         summary = 0
-        for account in self.get_account_response().json():
+        for account in self.get_accounts_response().json():
             try:
                 summary += account['summary'][0]['balance']
             except IndexError:
                 pass
         return int(summary / 1000)
 
-    def get_lametric_frame(self):
-        return dict(icon=self.icon, goalData=self._get_goal_data_for_lametric_frame(), duration=self.duration)
+    def _get_income_transactions_sum_in_current_year(self) -> int:  # in thousands
+        summ = 0
+        for value in self._get_income_transactions_sum_by_month_in_current_year().values():
+            summ += value
+        return int(summ / 1000)
 
-    def _get_goal_data_for_lametric_frame(self):
-        return dict(start=self.goal_start, current=self.get_account_summary(), end=self.goal_end, unit=self.unit)
+    def _get_income_transactions_sum_by_month_in_current_year(self) -> dict:
+        transactions_by_month = {}
+        for n in self.MONTHS_NUMBERS:
+            transactions_by_month[n] = 0
+        current_year = datetime.today().year
+
+        for transaction in self.get_transactions_response().json():
+            value, date = transaction['value'], datetime.strptime(transaction['date'], '%Y-%m-%d %H:%M:%S')
+            if date.year == current_year and value > 0:
+                transactions_by_month[date.month] += value
+        return transactions_by_month
 
 
 class FramesCatalog:
@@ -66,7 +104,7 @@ class FramesCatalog:
     biz_bunch = [FinologBiz(**kwargs) for kwargs in settings.FINOLOG_BIZ_SETTINGS]
 
     def get_frames_data(self):
-        frames = [biz.get_lametric_frame() for biz in self.biz_bunch]
+        frames = [frame for sublist in [biz.get_lametric_frames() for biz in self.biz_bunch] for frame in sublist]
         return dict(frames=frames)
 
     def get_frames_json(self):
--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ PORT = 8088
 FINOLOG_API_KEY = 'dkv6uaV7ON2fEThB18318e4d73b5a02e35a526cc0fa39defnzLed5sqZyV775ua'
 
 FINOLOG_BIZ_SETTINGS = [
-    {'biz_id': 28618, 'icon': 18348, 'goal_start': 0, 'goal_end': 26000, 'unit': 'K', 'duration': 3},  # Beer
-    {'biz_id': 28615, 'icon': 33859, 'goal_start': 0, 'goal_end': 3500, 'unit': 'K', 'duration': 3}]  # Develop
+    {'biz_id': 28618, 'icon': 18348, 'goal_start': 0, 'goal_end': 26000, 'unit': 'K', 'duration': 30},  # Beer
+    {'biz_id': 28615, 'icon': 33859, 'goal_start': 0, 'goal_end': 3500, 'unit': 'K', 'duration': 30}]  # Develop
